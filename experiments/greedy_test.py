c3c815168afc6ce9bc02629e30660e13b63c0bd9
Vicente Almeida <vicente.nejar@gmail.com>
2024-05-11 16:36:59 -0300

    Colorful sunburst

--- experiments/greedy_test.py
+++ experiments/greedy_test.py
@@ -29,7 +29,7 @@ movie_dataset = Dataset(
 
 movie_hypotheses = [
     HypothesisTest(aggregation='mean', null_value=4, alternative='greater', n_sample=HypothesisTest.ONE_SAMPLE),
-    HypothesisTest(aggregation='mean', null_value=3.5, alternative='less', n_sample=HypothesisTest.ONE_SAMPLE),
+    # HypothesisTest(aggregation='mean', null_value=3.5, alternative='less', n_sample=HypothesisTest.ONE_SAMPLE),
 ]
 
 eta = 1
@@ -42,6 +42,16 @@ initial_wealth = eta * alpha
 
 datasets = {
     'MovieLens': (movie_dataset, movie_hypotheses),
+}
+
+color_mapping = {
+    'genre': '#636EFA',
+    'runtime_minutes': '#EF553B',
+    'year': '#00CC96',
+    'gender': '#AB63FA',
+    'age': '#FFA15A',
+    'occupation': '#19D3F3',
+    'location': '#FF6692',
 }
 
 for dataset_name, dataset_variables in datasets.items():
@@ -63,14 +73,37 @@ for dataset_name, dataset_variables in datasets.items():
 
     greedy.reset()
 
+    ids = []
+    labels = []
+    color_sequence = []
+    parents = []
+
     for step in range(m):
         selected_g_in, selected_G_out, selected_h = greedy.step()
         print(selected_h)
         print(str(selected_g_in) + ' - ' + str(len(selected_g_in.user_ids)) + 'users')
+
+        
+        if len(parents) == 0:
+            ids.append(','.join(selected_g_in.predicates.values()))
+            color_sequence.append(color_mapping[list(selected_g_in.predicates.keys())[-1]])
+            labels.append(list(selected_g_in.predicates.values())[-1])
+            parents.append('')
+            
+        
         for g in selected_G_out:
             print('\t' + str(g) + ' - ' + str(len(g.user_ids)) + 'users')
+            ids.append(','.join(g.predicates.values()))
+            labels.append(list(g.predicates.values())[-1])
+            color_sequence.append(color_mapping[list(g.predicates.keys())[-1]])
+            parents.append(','.join(selected_g_in.predicates.values()))
         print()
 
         cov = coverage(selected_G_out, selected_g_in)
         div = diversity(selected_G_out, normalized=False)
         div_norm = diversity(selected_G_out, normalized=True)
+    
+    print(f'ids = {ids}')
+    print(f'labels = {labels}')
+    print(f'color_sequence = {color_sequence}')
+    print(f'parents = {parents}')
